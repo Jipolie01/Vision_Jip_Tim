3707be829c855b511f98bce9e639730570dcd955
Tim IJntema <klimtim@hotmail.com>
2017-02-19 15:26:51 +0100

    RGB imageshell is finished

--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -1,76 +1,51 @@
 #include "RGBImageStudent.h"
 
 RGBImageStudent::RGBImageStudent() : RGBImage() {
-	int throwError = 0, e = 1 / throwError; //Throws error without the need to include a header
-	//TODO: Nothing
 }
 
-RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
+RGBImageStudent::RGBImageStudent(const RGBImageStudent & other) : RGBImage(other.getWidth(), other.getHeight()) {
+	image_colors.clear();
+	for (const auto & color: other.image_colors) {
+		image_colors.push_back(color);
+	}
 }
 
 
 RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
+	image_colors.resize(width * height);
 }
 
 RGBImageStudent::~RGBImageStudent() {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: delete allocated objects
+	//vector deleten??
 }
 
 void RGBImageStudent::set(const int width, const int height) {
 	RGBImage::set(width, height);
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
+	
+	image_colors.resize(width * height);
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
 	RGBImage::set(other.getWidth(), other.getHeight());
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
+	
+	image_colors.clear();
+	for (const auto & color : other.image_colors) {
+		image_colors.push_back(color);
+	}
 }
 
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	image_colors[y*getWidth() + x] = pixel;
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
-	int throwError = 0, e = 1 / throwError;
-	/*
-	* TODO: set pixel i in "Row-Major Order"
-	*
-	*
-	* Original 2d image (values):
-	* 9 1 2
-	* 4 3 5
-	* 8 7 8
-	*
-	* 1d representation (i, value):
-	* i		value
-	* 0		9
-	* 1		1
-	* 2		2
-	* 3		4
-	* 4		3
-	* 5		5
-	* 6		8
-	* 7		7
-	* 8		8
-	*/
+	image_colors[i] = pixel;
 }
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
-	return 0;
+	return image_colors[y*getWidth() + x];
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: see setPixel(int i, RGB pixel)
-	return 0;
+	return image_colors[i];
 }
--- source/ExternalDLL/ExternalDLL/RGBImageStudent.h
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.h
@@ -4,9 +4,21 @@
 * Proprietary and confidential
 */
 
+/*
+Information:
+	http://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-in-c-using-new
+	http://lemire.me/blog/2012/06/20/do-not-waste-time-with-stl-vectors/
+*/
+
 #pragma once
 #include "RGBImage.h"
+
+#include <vector>
+typedef std::vector<RGB> rgb_vector;
+
 class RGBImageStudent : public RGBImage {
+private:
+	rgb_vector image_colors;
 public:
 
 	RGBImageStudent();
@@ -15,7 +27,7 @@ public:
 	~RGBImageStudent();
 
 	void set(const int width, const int height);
-	void set(const RGBImageStudent &other);
+	void set(const RGBImageStudent & other);
 
 	void setPixel(int x, int y, RGB pixel);
 	void setPixel(int i, RGB pixel);
